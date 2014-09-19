8efba281e5fc892fa038bb986ed7171dacc58289
Joe Sak <joe@joesak.com>
2014-09-18 20:42:04 -0400

    Clean up weird controller abstraction

--- app/controllers/group_sessions_controller.rb
+++ app/controllers/group_sessions_controller.rb
@@ -3,72 +3,66 @@ class GroupSessionsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
 
   def index
-    @live_sessions = load_sessions('starts_at <= ?', Time.current)
+    @live_sessions = group_session_scope.live
     @booked_sessions = current_user.booked_sessions
-    @upcoming_sessions = load_sessions('starts_at > ?', Time.current)
+    @upcoming_sessions = group_session_scope.upcoming
   end
 
   def show
-    load_session
+    @group_session = group_session_scope.find(params[:id])
   end
 
   def book
-    load_session
+    @group_session = group_session_scope.find(params[:id])
     @group_session.add_participant(current_user)
     flash[:info] = t('controllers.group_sessions.book.successful')
     redirect_to :back
   end
 
   def new
-    build_session
+    @group_session = group_session_scope.new
   end
 
   def create
-    build_session
-    save_session or render :new
-  end
-
-  def edit
-    load_session
-  end
-
-  def update
-    load_session
-    build_session
-    save_session(redirect: group_session_path(@group_session)) or render :edit
-  end
-
-  def destroy
-    load_session
-    destroy_session
-  end
-
-  private
-  def build_session
-    @group_session ||= group_scope.new
-    @group_session.attributes = group_session_params
-  end
-
-  def save_session(options = { redirect: root_path })
+    @group_session = group_session_scope.new(group_session_params)
     if @group_session.save
-      redirect_to options[:redirect]
+      flash[:info] = t('controllers.group_sessions.create.successful')
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
-  def load_session
-    @group_session = group_scope.find(params[:id])
+  def edit
+    @group_session = group_session_scope.find(params[:id])
+    unless @group_session.host == current_user
+      flash[:alert] = t('controllers.application.unauthorized')
+      redirect_to root_path
+    end
   end
 
-  def load_sessions(*args)
-    @group_sessions = group_scope.where(*args)
+  def update
+    @group_session = group_session_scope.find(params[:id])
+    if @group_session.update_attributes(group_session_params)
+      redirect_to @group_session
+    else
+      render :edit
+    end
   end
 
-  def destroy_session
-    @group_session.destroy
+  def destroy
+    @group_session = group_session_scope.find(params[:id])
+    if @group_session.host == current_user
+      @group_session.destroy
+      flash[:info] = t('controllers.group_sessions.destroy.successful')
+    else
+      flash[:alert] = t('controllers.application.unauthorized')
+    end
     redirect_to root_path
   end
 
-  def group_scope
+  private
+  def group_session_scope
     GroupSession.not_deleted.where(nil)
   end
 
