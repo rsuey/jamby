555ceb6aff4e0abbc28bd9f2d69dca5ea3941e84
Joe Sak <joe@joesak.com>
2014-09-21 18:20:38 -0400

    Show group session details on confirm payment page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,23 +2,23 @@ class PaymentsController < ApplicationController
   before_filter :authenticate_user!
 
   def confirm
-    group_session = GroupSession.find(params[:id])
-    @payment = Payment.new(group_session: group_session, user: current_user)
+    @group_session = GroupSession.find(params[:id])
+    @payment = Payment.new(group_session: @group_session, user: current_user)
   end
 
   def create
-    group_session = GroupSession.find(payment_params[:group_session_id])
+    @group_session = GroupSession.find(payment_params[:group_session_id])
     payment_method = current_user.payment_methods
                                  .find(payment_params[:payment_method_id])
     @payment = Payment.new(payment_method: payment_method,
-                           group_session: group_session,
+                           group_session: @group_session,
                            user: current_user)
     if @payment.save
-      group_session.add_participant(current_user)
+      @group_session.add_participant(current_user)
       flash[:info] = t('controllers.group_sessions.book.successful')
-      redirect_to group_session_path(group_session)
+      redirect_to group_session_path(@group_session)
     else
-      render :new
+      render :confirm
     end
   end
 
