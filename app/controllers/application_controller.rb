ea23bb4bcb836ab7a69567286946cc121fb231a4
Joe Sak <joe@joesak.com>
2014-09-21 16:17:42 -0400

    Remove unecessary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
     @current_user ||= Guest.new
   end
 
