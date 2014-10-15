28d9eb626fc072e89dcc2894d11d99dc2ffe5459
Joe Sak <joe@joesak.com>
2014-10-15 02:25:59 -0400

    Forget the VCR option, this is faster

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,5 +54,4 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.ignore_localhost = true
-  c.default_cassette_options = { record: :new_episodes }
 end
