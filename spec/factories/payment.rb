48ebd1cd51b1d15af7124d6d859bcbe1ab08c515
Joe Sak <joe@joesak.com>
2014-10-15 02:00:08 -0400

    Clarify, optimize, and cut down the size of GroupSession spec

--- spec/factories/payment.rb
+++ spec/factories/payment.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
   factory :payment do
   end
+
+  factory :deleted_payment, parent: :payment do
+    deleted_at Time.current
+  end
 end
--- spec/models/group_session_spec.rb
+++ spec/models/group_session_spec.rb
@@ -27,10 +27,13 @@ describe GroupSession do
     expect(group_session.payout_value).to eq(0)
   end
 
-  it 'returns host email' do
-    user = create(:user, email: 'host@example.com')
-    group_session = create(:group_session, host: user)
+  it 'delegates name, avatar, and email to the host' do
+    user = create(:user, first_name: 'Foo', last_name: 'Bar',
+                         email: 'host@example.com')
+    group_session = build(:group_session, host: user)
     expect(group_session.host_email).to eq('host@example.com')
+    expect(group_session.host_name).to eq('Foo Bar')
+    expect(group_session.host_avatar).not_to be_nil
   end
 
   it 'uses a hashed ID' do
@@ -42,188 +45,111 @@ describe GroupSession do
   it 'is not fully booked before 10 bookings' do
     group_session = create(:group_session)
     expect(group_session).not_to be_fully_booked
-    9.times { Booking.create(group_session, create(:user)) }
-    expect(group_session).not_to be_fully_booked
-  end
 
-  it 'is fully booked at 10 bookings' do
-    group_session = create(:group_session)
-    10.times { Booking.create(group_session, create(:user)) }
-    expect(group_session).to be_fully_booked
+    9.times { Booking.create(group_session, create(:user)) }
+    expect(group_session.reload).not_to be_fully_booked
+
+    Booking.create(group_session, create(:user))
+    expect(group_session.reload).to be_fully_booked
   end
 
   it 'refunds users the difference when the price is lowered' do
-    group_session = create(:group_session, price: 2)
-    create(:payment, group_session: group_session)
+    VCR.use_cassette('refund difference on lowered price') do
+      group_session = create(:group_session, price: 2)
+      payment_method = create(:payment_method)
+      account = create(:account)
+      payment = group_session.payments.create(amount: 200,
+                                              account: account,
+                                              payment_method: payment_method)
 
-    expect_any_instance_of(Payment).to receive(:refund).with(100)
-    group_session.update_attributes(price: 1)
+      Booking.create(group_session, account)
+      group_session.update_attributes(price: 1)
+
+      email = ActionMailer::Base.deliveries.last
+      expect(email.to).to eq([account.email])
+      expect(email.subject).to eq('Price reduced')
+      expect(payment.reload.remote_refund_id).not_to be_nil
+    end
   end
 
-  it 'emails the participant when the price is lowered' do
+  it 'returns the youtube embed path' do
+    group_session = build(:group_session, broadcast_id: 'fooBar')
+    expect(group_session.broadcast_embed).to eq('//youtube.com/embed/fooBar')
+  end
+
+  it 'is #live_details_ready? if live_url and broadcast_id are present' do
+    group_session = build(:group_session)
+    expect(group_session).to_not be_live_details_ready
+
+    group_session = build(:group_session, live_url: 'present',
+                                          broadcast_id: 'here')
+    expect(group_session).to be_live_details_ready
+  end
+
+  it 'is paid when it is free' do
+    group_session = build(:free_group_session)
+    expect(group_session).to be_paid(double(:account))
+  end
+
+  it 'is not paid when it is priced and no payments' do
+    group_session = build(:priced_group_session)
+    expect(group_session).to_not be_paid(double(:account))
+  end
+
+  it 'is paid by users who have paid for it' do
+    account = create(:account)
+    group_session = create(:priced_group_session)
+    create(:payment, group_session: group_session, account: account)
+    expect(group_session).to be_paid(account)
+  end
+
+  it 'is not paid if the user payment is deleted' do
+    account = create(:account)
+    group_session = create(:priced_group_session)
+    create(:deleted_payment, group_session: group_session, account: account)
+    expect(group_session).to_not be_paid(account)
+  end
+
+  it 'can generally be booked' do
+    user = double(:user)
+    group_session = build(:group_session)
+    expect(group_session).to be_bookable_by(user)
+  end
+
+  it 'cannot be booked by the host' do
     user = create(:user)
-    group_session = create(:group_session, price: 2)
+    group_session = build(:group_session, host: user)
+    expect(group_session).not_to be_bookable_by(user)
+  end
+
+  it 'cannot be double booked' do
+    user = create(:user)
+    group_session = create(:group_session)
 
     Booking.create(group_session, user)
-
-    expect {
-      group_session.reload.update_attributes(price: 1)
-    }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    expect(group_session).not_to be_bookable_by(user)
   end
 
-  describe '#broadcast_embed' do
-    it 'returns the youtube embed path' do
-      group_session = create(:group_session, broadcast_id: 'fooBar')
-      expect(group_session.broadcast_embed).to eq('//youtube.com/embed/fooBar')
-    end
+  it 'is not booked out of thin air' do
+    group_session = build(:group_session)
+    user = create(:user)
+    expect(group_session).to_not be_booked_by(user)
   end
 
-  describe '#live_details_ready?' do
-    it 'returns true when live_url and broadcast_id are present' do
-      group_session = create(:group_session, live_url: 'present',
-                                             broadcast_id: 'here')
-      expect(group_session).to be_live_details_ready
-    end
-
-    it 'returns false when live_url and broadcast_id are not present' do
-      group_session = create(:group_session)
-      expect(group_session).to_not be_live_details_ready
-    end
+  it 'is booked by users creating Bookings' do
+    group_session = create(:group_session)
+    user = create(:user)
+    Booking.create(group_session, user)
+    expect(group_session).to be_booked_by(user)
   end
 
-  describe '#paid?' do
-    context 'when it is free' do
-      it 'returns true' do
-        group_session = create(:free_group_session)
-        expect(group_session).to be_paid(double(:user))
-      end
-    end
-
-    context 'when it is not free' do
-      context 'and the user does not have a payment' do
-        it 'returns false' do
-          group_session = create(:priced_group_session)
-          expect(group_session).to_not be_paid(double(:user))
-        end
-      end
-
-      context 'and the user has a payment' do
-        it 'returns true' do
-          user = create(:account)
-          group_session = create(:priced_group_session)
-          create(:payment, group_session: group_session, account: user)
-          expect(group_session).to be_paid(user)
-        end
-      end
-
-      context 'and the payment has been deleted' do
-        it 'returns false' do
-          user = create(:account)
-          group_session = create(:priced_group_session)
-          create(:payment, group_session: group_session, account: user)
-          Payment.last.destroy
-          expect(group_session).to_not be_paid(user)
-        end
-      end
-    end
+  it 'is free when the price is 0' do
+    group_session = build(:free_group_session)
+    expect(group_session).to be_free
   end
 
-  describe '#host_name' do
-    it 'returns the name of the host' do
-      user = create(:user, first_name: 'Foo', last_name: 'Bar')
-      group_session = create(:group_session, host: user)
-
-      expect(group_session.host_name).to eq('Foo Bar')
-    end
-  end
-
-  describe 'can_be_booked_by?' do
-    context 'when the user is the host' do
-      it 'returns false' do
-        user = create(:user)
-        group_session = create(:group_session, host: user)
-        expect(group_session.can_be_booked_by?(user)).to eq(false)
-      end
-    end
-
-    context 'when the user is on the guest list' do
-      it 'returns false' do
-        user = create(:user)
-        group_session = create(:group_session)
-
-        Booking.create(group_session, user)
-        expect(group_session.can_be_booked_by?(user)).to eq(false)
-      end
-    end
-
-    context 'when the user is not the host, not guest listed' do
-      it 'returns true' do
-        user = double(:user)
-        group_session = create(:group_session)
-        expect(group_session.can_be_booked_by?(user)).to eq(true)
-      end
-    end
-  end
-
-  describe '#booked_by?' do
-    context 'when the participant has not booked the session' do
-      it 'returns false' do
-        group_session = create(:group_session)
-        user = create(:user)
-        expect(group_session).to_not be_booked_by(user)
-      end
-    end
-
-    context 'when the participant has booked the session' do
-      it 'returns true' do
-        group_session = create(:group_session)
-        user = create(:user)
-        Booking.create(group_session, user)
-        expect(group_session).to be_booked_by(user)
-      end
-    end
-  end
-
-  describe '#free?' do
-    context 'when the price is 0' do
-      it 'returns true' do
-        group_session = create(:free_group_session)
-        expect(group_session).to be_free
-      end
-    end
-
-    context 'when the price is greater than 0' do
-      it 'returns false' do
-        group_session = create(:priced_group_session)
-        expect(group_session).to_not be_free
-      end
-    end
-  end
-
-  # https://github.com/JackDanger/permanent_records/
-  describe 'Soft deletion' do
-    describe '.destroy_all' do
-      it 'updates deleted_at on all group sessions' do
-        datetime = Time.current
-        3.times { create(:group_session) }
-
-        Timecop.freeze(datetime) do
-          GroupSession.destroy_all
-          expect(GroupSession.all.map(&:deleted_at).uniq).to eq([datetime])
-        end
-      end
-    end
-
-    describe '#destroy' do
-      it 'updates deleted_at on the group session' do
-        datetime = Time.current
-        group_session = create(:group_session)
-
-        Timecop.freeze(datetime) do
-          group_session.destroy
-          expect(group_session.deleted_at).to eq(datetime)
-        end
-      end
-    end
+  it 'is not free when the group session is priced' do
+    group_session = build(:priced_group_session)
+    expect(group_session).to_not be_free
   end
 end
