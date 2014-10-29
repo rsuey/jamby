b5ed3444f74182f38d56d51250fe84b057889876
Joe Sak <joe@joesak.com>
2014-10-29 08:16:08 -0700

    Use the default url option in the paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -15,3 +15,5 @@ Paperclip.interpolates(:gravatar_url) do |attachment, style|
   # and default is second
   attachment.instance.gravatar_url(nil, size)
 end
+
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
